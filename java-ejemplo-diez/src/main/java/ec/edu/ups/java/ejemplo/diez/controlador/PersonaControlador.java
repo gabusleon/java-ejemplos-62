006eb8209ccd5810ac35a2c7ade807a167b43191
Gabriel León Paredes <gabrielleonp@hotmail.com>
2023-06-09 11:19:22 -0500

    Sesión del 09/junio/23 se actualiza ejemplo para agenda telefonica

---
+++ java-ejemplo-diez/src/main/java/ec/edu/ups/java/ejemplo/diez/controlador/PersonaControlador.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.java.ejemplo.diez.controlador;
+
+import ec.edu.ups.java.ejemplo.diez.idao.IPersonaDAO;
+import ec.edu.ups.java.ejemplo.diez.modelo.Persona;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author PhD. Gabriel A. León Paredes
+ */
+public class PersonaControlador {
+    private Persona persona;
+    private IPersonaDAO personaDAO;
+
+    public PersonaControlador(IPersonaDAO personaDAO) {        
+        this.personaDAO = personaDAO;
+    }
+    
+    public void crear(Persona persona){
+        this.persona = persona;
+        personaDAO.create(persona);
+    }
+                
+}
